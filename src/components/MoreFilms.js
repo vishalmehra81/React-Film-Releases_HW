f08bc12cb1479b11db6987879b7dedd5be84f4a8
Vishal Mehra <67109899+vishalmehra81@users.noreply.github.com>
2020-10-13 07:52:06 +0100

    minor updates

--- src/components/MoreFilms.js
+++ src/components/MoreFilms.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function MoreFilms(){
     return (
         <div className="more_films">
-            <a href="https://www.imdb.com/calendar/?region=gb">VIEW MORE UPCOMING RELEASES>></a>
+            <a href="https://www.imdb.com/calendar/?region=gb">VIEW MORE UPCOMING RELEASES...</a>
         </div>
     )
 }
